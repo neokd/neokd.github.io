4f3d5098ee616b379529aa966e2759b9e710bc30
Kuldeep <71772185+neokd@users.noreply.github.com>
2022-02-25 23:03:12 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbygFEReVPcQZkNhFJ_DkpBRlI-a3fNc0FmGKr8hCfNcoQ25cRfn_YhmLjIhbTZu7H0c/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+    alert("Form Submitted Successfully")
+})
+
